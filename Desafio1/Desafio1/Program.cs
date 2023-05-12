9e5707def275c211016c2c973e9b584141e5b82b
Paulo Lisboa <paulolisboa38@gmail.com>
2023-05-12 17:17:25 -0300

    2 - Desafio do Palíndromo:

--- Desafio1/Desafio1/Program.cs
+++ Desafio1/Desafio1/Program.cs
@@ -10,56 +10,29 @@ namespace Desafio1
     {
         static void Main(string[] args)
         {
-            string palavra = "sim";
-            //string resposta = "_____";
+            Console.Write("Digite uma palavra: ");
+            string paraVerificar = Console.ReadLine();
+            int count = paraVerificar.Length;
+            char[] aux = new char[count];
+            aux = paraVerificar.ToCharArray();
 
-            char[] letras = new char[palavra.Length];
-            //letras = palavra.ToCharArray();
-            for (int i = 0; i < letras.Length; i++)
+            bool flag = false;
+
+            for (int i = 1; i <= count; i++)
             {
-                letras[i] = '_';
+                if (aux[i - 1] == aux[count - i])
+                {
+                    flag = true;
+                }
             }
 
-            bool flag = true;
-
-            while (flag)
+            if (flag == false)
             {
-                Console.Write(" Insira uma letra apenas: ");
-                char c = char.Parse(Console.ReadLine());
-
-                if (c == 's')
-                {
-                    letras[0] = 's';
-                    Console.WriteLine();
-                }
-                else if (c == 'i')
-                {
-                    letras[1] = 'i';
-                    Console.WriteLine();
-                }
-                else if (c == 'm')
-                {
-                    letras[2] = 'm';
-                    Console.WriteLine();
-                }
-
-                Console.Write("-");
-                foreach (var item in letras)
-                {
-                    Console.Write(item);
-                }
-                Console.Write("-");
-
-                if (letras[0] == 's' && letras[1] == 'i' && letras[2] == 'm')
-                {
-                    flag = false;
-                }
-
-                else if (flag == false)
-                {
-                    break;
-                }
-                Console.WriteLine();
+                Console.WriteLine($"A string {paraVerificar} não é um palíndromo.");
+            }
+            else
+            {
+                Console.WriteLine($"A string {paraVerificar} é um palíndromo.");
             }
         }
     }
